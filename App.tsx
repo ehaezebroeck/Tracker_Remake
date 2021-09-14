83b92f71b6e7569ea19526da9e14d246a59565f1
Ethan Haezebroeck <haezebroecke@gmail.com>
2021-09-14 17:01:51 -0500

    Created square grid manually

--- App.tsx
+++ App.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Square } from 'react-native-shape';
 
 const YourApp = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>
-        Try editing me! 🎉
-      </Text>
+    <View style={{flex: 1, flexDirection: "column", justifyContent: "space-evenly"}}>
+      <View style={{flex: 1, flexDirection: "row", justifyContent: "space-evenly", alignItems: "center"}}>
+        <Square color="red"/>
+        <Square color="red"/>
+        <Square color="red"/>
+      </View>
+      <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-evenly", alignItems: "center"}}>
+      <Square color="blue"/>
+        <Square color="blue"/>
+        <Square color="blue"/>
+      </View>
+      <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-evenly", alignItems: "center"}}>
+      <Square color="green"/>
+        <Square color="green"/>
+        <Square color="green"/>
+      </View>
     </View>
   );
 }
+
+// const SquareGrid = (rows, cols) => {
+//   for ()
+// }
 
 export default YourApp;
